64461ab388077baba5e57a83fddf689ee0945d72
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2018-05-21 16:18:56 +0200

    Re-implemented convert thread to fiber with kernel lists

--- module/include/core.h
+++ module/include/core.h
@@ -16,6 +16,7 @@
 
 #include "common.h"
 #include "ioctlcmd.h"
+#include "utils.h"
 #include <asm/current.h>
 #include <asm/ptrace.h>
 #include <linux/fs.h>
@@ -29,7 +30,7 @@
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 
-#define CORE_LOG ": DEV: "
+#define CORE_LOG ": CORE: "
 
 /*
  * Definitions
@@ -47,7 +48,7 @@ typedef struct fiber_params fiber_params_t;
 
 int convert_thread_to_fiber(void);
 int create_fiber(fiber_params_t *params);
-int switch_to_fiber(int fiber_id);
+int switch_to_fiber(unsigned fid);
 
 /**
  * @brief Params to be passed by the library when creating or converting to a fiber
@@ -70,47 +71,13 @@ typedef enum fiber_state {
 } fiber_state_t;
 
 /**
- * @brief A node of the list of processes in @ref fibered_processes_list, a **fiber-enabled**
- * process
- *
- * A @ref fibered_process is a process in which at least one thread has been converted to a fiber,
- * so a fibered_process always has at least one fiber in the fibered_process::fibers_list field.
- * Moreover the first element of the list has fibered_process::prev pointing to @c NULL and the last
- * element of the list has fibered_process::next pointing to @c NULL.
- *
- */
-typedef struct fibered_process {
-    fibered_process_node_t *prev; /**< The previous element of the list */
-    fibered_process_node_t *next; /**< The next element of the list */
-    fibers_list_t *fibers_list; /**< A list of fibers created in the process environment (so by any
-                                   thread in it) */
-    pid_t pid; /**< the pid of the main thread, so the tgid of every thread of the process */
-} fibered_process_node_t;
-
-/**
- * @brief The list of processes that have at least one fiber
- *
- * This list has the purpose of containing the list of processes that have created at least one
- * fiber and they became fiber-enabled. The list can be considered empty when at least one of
- * fibered_processes_list::head or fibered_processes_list::tail is pointing to @c NULL.
- *
- */
-typedef struct fibered_processes_list {
-    fibered_process_node_t *head; /**< The first element in the list */
-    fibered_process_node_t *tail; /**< The last element in the list */
-    unsigned processes_count;     /**< The number of elements in the list */
-} fibered_processes_list_t;
-
-/**
  * @brief A node in the @ref fibers_list. This type fully represent a @c fiber
  *
  * This is the complete description of a fiber.
  *
  */
 typedef struct fiber {
-    fiber_node_t *prev;                 /**< Pointer to the previous element in the list */
-    fiber_node_t *next;                 /**< Pointer to the next element in the list */
-    unsigned id;                        /**< Unique if of the fiber, used for  */
+    unsigned id;                        /**< Unique if of the fiber, used for ... */
     unsigned long starting_function;    /**< The starting fuction of the fiber */
     unsigned long base_user_stack_addr; /** The starting address of the stack that will be
                                               allocated by the library */
@@ -129,21 +96,47 @@ typedef struct fiber {
     unsigned success_activations_count; /** Number of successful activation of the fiber */
     unsigned failed_activations_count;  /** Number of failed activation of the fiber */
     unsigned long total_time;           /** Total running time of the fiber */
+    struct list_head list;              /**< List implementation structure */
 } fiber_node_t;
 
 /**
  * @brief A generic list of fibers
  *
- * The list has the purpose of containing the list of fibers for a given process.  The list can be
- * considered empty when at least one of fibers_list::head or fibers_list::tail is pointing to @c
- * NULL.
+ * The list has the purpose of containing the list of fibers for a given process.
  *
  */
 typedef struct fibers_list {
-    fiber_node_t *head;    /**< The first element in the list */
-    fiber_node_t *tail;    /**< The last element in the list */
+    struct list_head list;
     unsigned fibers_count; /**< Number of fibers created */
-
 } fibers_list_t;
+
+/**
+ * @brief A node of the list of processes in @ref fibered_processes_list, a **fiber-enabled**
+ * process
+ *
+ * A @ref fibered_process is a process in which at least one thread has been converted to a fiber,
+ * so a fibered_process always has at least one fiber in the fibered_process::fibers_list field.
+ * Moreover the first element of the list has fibered_process::prev pointing to @c NULL and the last
+ * element of the list has fibered_process::next pointing to @c NULL.
+ *
+ */
+typedef struct fibered_process {
+    pid_t pid; /**< the pid of the main thread, so the tgid of every thread of the process */
+    fibers_list_t fibers_list; /**< A list of fibers created in the process environment (so by any
+                                  thread in it) */
+    struct list_head list;     /**< List implementation structure */
+} fibered_process_node_t;
+
+/**
+ * @brief The list of processes that have at least one fiber
+ *
+ * This list has the purpose of containing the list of processes that have created at least one
+ * fiber and they became fiber-enabled.
+ *
+ */
+typedef struct fibered_processes_list {
+    struct list_head list;
+    unsigned processes_count; /**< The number of elements in the list */
+} fibered_processes_list_t;
 
 #endif
--- module/src/core.c
+++ module/src/core.c
@@ -16,7 +16,7 @@
 /**
  * @brief The variable of the core part that will contain the **fiber-enabled** processes
  *
- */
+
 // clang-format off
 static fibered_processes_list_t fibered_processes_list = {
     .head = NULL,
@@ -24,6 +24,12 @@ static fibered_processes_list_t fibered_processes_list = {
     .processes_count = 0
 };
 // clang-format on
+*/
+
+static fibered_processes_list_t fibered_processes_list_head = {
+    .list = LIST_HEAD_INIT(fibered_processes_list_head.list),
+    .processes_count = 0,
+};
 
 /*
  * Implementations
@@ -33,22 +39,22 @@ static fibered_processes_list_t fibered_processes_list = {
  * @brief Convert the current thread to a fiber
  *
  * # Implementation
- * When a thread is converted to a fiber several tasks are performed. First of all, we must check if
- * the process if *fiber-enabled* or not by searching if its `pid` (so even its `tgid`) is already
- * present in the fibered_processes_list, that must be initialized if it is the case.
+ * When a thread is converted to a fiber several tasks are performed. First of all, we must
+ * check if the process if *fiber-enabled* or not by searching if its `pid` (so even its `tgid`)
+ * is already present in the fibered_processes_list, that must be initialized if it is the case.
  *
  * ## Process not **fiber-enabled**
- * If the process never created a fiber, it must become **fiber-enabled**, this means that we have
- * to instantiate a fibered_process element in the @ref fibered_processes_list variable. Then we
- * have to instantiate a @ref fiber element in the fibers_list field of the fibered_process element
- * of the list.
+ * If the process never created a fiber, it must become **fiber-enabled**, this means that we
+ * have to instantiate a fibered_process element in the @ref fibered_processes_list variable.
+ * Then we have to instantiate a @ref fiber element in the fibers_list field of the
+ * fibered_process element of the list.
  *
  * ## Process **fiber-enabled**
- * If the process is already a *fiber-enabled* then we have just to append a new @ref fiber entry to
- * the fibered_process::fibers_list. This requires that the parameters are also set according to the
- * ones that are passed to the function as @p params argument. At the end a new proc file must be
- * created in the directory `/proc/<pid>/fibers/<fid>`. The fields of the fiber element are set in
- * this way:
+ * If the process is already a *fiber-enabled* then we have just to append a new @ref fiber
+ * entry to the fibered_process::fibers_list. This requires that the parameters are also set
+ * according to the ones that are passed to the function as @p params argument. At the end a new
+ * proc file must be created in the directory `/proc/<pid>/fibers/<fid>`. The fields of the
+ * fiber element are set in this way:
  * - fiber::id is set to the current number of fibers -1;
  * - fiber::regs is set with the function `task_pt_regs(current)`;
  * - fiber::starting_function is set to fiber::regs::ip;
@@ -59,74 +65,41 @@ static fibered_processes_list_t fibered_processes_list = {
  * - fiber::total_time is set to 0;
  * - fiber::base_user_stack_addr is ignored;
  *
- * @return int the id of the newly created fiber otherwise `ERR_THREAD_ALREADY_FIBER` if the thread
- * already has been converted to a fiber
+ * @return int the id of the newly created fiber otherwise `ERR_THREAD_ALREADY_FIBER` if the
+ * thread already has been converted to a fiber
  */
 int convert_thread_to_fiber() {
-    // check if process already created at least a fiber
-    fibered_process_node_t *fibered_process_node = fibered_processes_list.head;
+    fibered_process_node_t *fibered_process_node;
     fiber_node_t *fiber_node;
-    uint8_t found = 0;
-    while (fibered_process_node != NULL) {
-        if (fibered_process_node->pid == current->tgid) {
-            found = 1;
-            break;
-        }
-        fibered_process_node = fibered_process_node->next;
-    }
-    // process has never created a fiber
-    if (!found) {
-        fibered_process_node = kmalloc(sizeof(fibered_process_node_t), GFP_KERNEL);
+    // check if process if fiber enabled
+    // check if process already created at least a fiber
+    check_if_exists(fibered_process_node, &fibered_processes_list_head.list, pid, current->tgid,
+                    list, fibered_process_node_t);
+    if (fibered_process_node == NULL) {
+        // process has never created a fiber
+        printk(KERN_DEBUG MODULE_NAME CORE_LOG "Fibered process does not exist");
+        create_list_entry(fibered_process_node, &fibered_processes_list_head.list, list,
+                          fibered_process_node_t);
+        fibered_processes_list_head.processes_count++;
         fibered_process_node->pid = current->tgid;
-        fibered_processes_list.processes_count = 1;
-        fibered_process_node->prev = NULL;
-        fibered_process_node->next = NULL;
-        fibered_process_node->fibers_list = kmalloc(sizeof(fibers_list_t), GFP_KERNEL);
-        fibered_process_node->fibers_list->fibers_count = 0;
-        fibered_process_node->fibers_list->head = NULL;
-        fibered_process_node->fibers_list->tail = NULL;
-        // if head is null we have to init the list
-        if (fibered_processes_list.head == NULL) fibered_processes_list.head = fibered_process_node;
-        // otherwise we append to the list
-        if (fibered_processes_list.tail != NULL) {
-            fibered_processes_list.tail->next = fibered_process_node;
-            fibered_process_node->prev = fibered_processes_list.tail;
-        }
-        fibered_processes_list.tail = fibered_process_node;
+        INIT_LIST_HEAD(&fibered_process_node->fibers_list.list);
+        fibered_process_node->fibers_list.fibers_count = 0;
     }
-    printk(KERN_DEBUG MODULE_NAME CORE_LOG "tgid is %d", fibered_process_node->pid);
-    // check if thread is already a fiber
-    fiber_node = fibered_process_node->fibers_list->head;
-    found = 0;
-    while (fiber_node != NULL) {
-        if (fiber_node->created_by == current->pid) {
-            found = 1;
-            break;
-        }
-        fiber_node = fiber_node->next;
-    }
-    if (!found) {
-        fiber_node = kmalloc(sizeof(fiber_node_t), GFP_KERNEL);
-        fiber_node->prev = NULL;
-        fiber_node->next = NULL;
+
+    // check if the thread is already a fiber
+    check_if_exists(fiber_node, &fibered_process_node->fibers_list.list, run_by, current->pid, list,
+                    fiber_node_t);
+    if (fiber_node == NULL) {
+        // thread is not a fiber
+        create_list_entry(fiber_node, &fibered_process_node->fibers_list.list, list, fiber_node_t);
+        fiber_node->id = fibered_process_node->fibers_list.fibers_count;
         fiber_node->created_by = current->pid;
-        fibered_process_node->fibers_list->fibers_count++;
-        if (fibered_process_node->fibers_list->head == NULL)
-            fibered_process_node->fibers_list->head = fiber_node;
-        if (fibered_process_node->fibers_list->tail != NULL) {
-            fibered_process_node->fibers_list->tail->next = fiber_node;
-            fiber_node->prev = fibered_process_node->fibers_list->tail;
-        }
-        fibered_process_node->fibers_list->tail = fiber_node;
+        fiber_node->run_by = current->pid;
+        fibered_process_node->fibers_list.fibers_count++;
+        fiber_node->state = RUNNING;
     } else
         return -ERR_THREAD_ALREADY_FIBER;
-
-    fiber_node->id = fibered_process_node->fibers_list->fibers_count - 1;
-    fiber_node->state = RUNNING;
-
-    // TODO Create proc now
-
-    return fiber_node->id;
+    return 0;
 }
 
 /**
