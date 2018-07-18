f8da93d187d16044465f2ccc2b10279fc73436a6
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2018-07-18 10:23:11 +0200

    Updated project name in licenses

--- library/src/fiber_example.c
+++ library/src/fiber_example.c
@@ -2,20 +2,20 @@
  * Copyright (C) 2018 Gabriele Proietti Mattia <gabry.gabry@hotmail.it> & Alexandru Daniel Tufa
  * <alex.tufa94@gmail.com>
  *
- * This file is part of Fiber (Library).
+ * This file is part of Fibers (Library).
  *
- * Fiber (Library) is free software: you can redistribute it and/or modify
+ * Fibers (Library) is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Fiber (Library) is distributed in the hope that it will be useful,
+ * Fibers (Library) is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Fiber (Library).  If not, see <http://www.gnu.org/licenses/>.
+ * along with Fibers (Library).  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
